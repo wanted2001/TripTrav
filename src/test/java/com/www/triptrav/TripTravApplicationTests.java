0b30c6333d3d6938f0e608db5b42f610a41521cf
ehdwo13 <ehdwo13@gmail.com>
2024-08-27 15:16:55 +0900

    init commit

---
+++ src/test/java/com/www/triptrav/TripTravApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.triptrav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripTravApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
