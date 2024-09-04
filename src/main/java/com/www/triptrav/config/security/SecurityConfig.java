a255398a804104a586830f61d1cd8f9c4f3b603e
haeunOh <25_asa@naver.com>
2024-09-04 21:45:32 +0900

    회원가입 html 연결

--- src/main/java/com/www/triptrav/config/security/SecurityConfig.java
+++ src/main/java/com/www/triptrav/config/security/SecurityConfig.java
@@ -1,4 +1,35 @@
 package com.www.triptrav.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/", "/home", "/dist/**").permitAll()
+//                        .requestMatchers("/user/**").authenticated()
+//                        .requestMatchers("/manager/**").hasAnyRole( "ADMIN")
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                );
+//                .formLogin(formLogin -> formLogin
+//                        .loginPage("/login")
+//                        .usernameParameter("username")
+//                        .passwordParameter("password")
+//                        .loginProcessingUrl("/user/login")
+//                        .defaultSuccessUrl("/")
+//               );
+
+        return http.build();
+    }
 }
