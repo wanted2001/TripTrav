5da0cbeb8278422aea987494be00d76de70ce34c
haeunOh <25_asa@naver.com>
2024-09-08 13:11:13 +0900

    회원가입 작업중

--- src/main/java/com/www/triptrav/controller/UserController.java
+++ src/main/java/com/www/triptrav/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.www.triptrav.controller;
 
+import com.www.triptrav.domain.UserVO;
 import com.www.triptrav.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/user/*")
@@ -13,5 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService usv;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/join")
+    public String joinUser(UserVO uvo, Model m){
+        log.info("user join start!");
+        log.info("join user >>> {}", uvo);
+        uvo.setPw(passwordEncoder.encode(uvo.getPw()));
+        usv.joinUser(uvo);
+        m.addAttribute("joinMsg","가입이 완료되었습니다!");
+        return "redirect:/?joinMsg=true";
+    }
 
 }
