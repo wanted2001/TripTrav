5667386797105713488c403b0f5fe6a70b61dc4c
haeunOh <25_asa@naver.com>
2024-09-10 17:55:37 +0900

    test

--- src/main/java/com/www/triptrav/config/SecurityConfig.java
+++ src/main/java/com/www/triptrav/config/SecurityConfig.java
@@ -1,69 +1,35 @@
 package com.www.triptrav.config;
 
-import com.www.triptrav.config.oauth2.PrincipalOauth2UserService;
-import com.www.triptrav.config.security.CustomAuthenticationFailureHandler;
-import com.www.triptrav.config.security.CustomAuthenticationSuccessHandler;
-import com.www.triptrav.config.security.CustomUserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final AuthenticationSuccessHandler OAuth2AuthenticationSuccessHandler;
-    private final PrincipalOauth2UserService principalOauth2UserService;
-    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
-    private final CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers("/**")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(login -> login
-                        .usernameParameter("email")
-                        .passwordParameter("pw")
-                        .loginProcessingUrl("/user/login")
-                        .successHandler(customAuthenticationSuccessHandler)
-                        .failureHandler(customAuthenticationFailureHandler)
-                        .permitAll()
-                )
-                .oauth2Login(oauth2->oauth2
-                        .loginPage("/")
-                        .successHandler(OAuth2AuthenticationSuccessHandler)
-                        .userInfoEndpoint(userInfo->userInfo
-                                .userService(principalOauth2UserService))
-                ) .logout(logout -> logout
-                        .logoutUrl("/user/logout")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID")
-                        .logoutSuccessUrl("/")
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/", "/home", "/dist/**").permitAll()
+//                        .requestMatchers("/user/**").authenticated()
+//                        .requestMatchers("/manager/**").hasAnyRole( "ADMIN")
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
                 );
+//                .formLogin(formLogin -> formLogin
+//                        .loginPage("/login")
+//                        .usernameParameter("username")
+//                        .passwordParameter("password")
+//                        .loginProcessingUrl("/user/login")
+//                        .defaultSuccessUrl("/")
+//               );
 
         return http.build();
     }
-
-    @Bean
-    UserDetailsService userDetailsService(){
-        return new CustomUserService();}
-
 }
