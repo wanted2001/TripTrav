41046654f1118f7ce89e22febd7efc91cc43468d
ehdwo13 <ehdwo13@gmail.com>
2024-09-17 18:45:19 +0900

    add review data to db function and modify html+css and fix image display css

--- src/main/resources/static/dist/js/place/placeDetail.js
+++ src/main/resources/static/dist/js/place/placeDetail.js
@@ -12,83 +12,50 @@ let mapx = 0;
 let mapy = 0;
 
 // 기본정보 가져오기
-fetch(detailInfoUrl)
-    .then(response => response.json())
-    .then(data => {
-        const jsonData = data.response.body.items.item[0];
-        const locationDiv = document.querySelector('.location');
-        let city = splitAddr(jsonData.addr1)[0];
-        let district = splitAddr(jsonData.addr1)[1];
-        let title = jsonData.title;
-        locationDiv.innerHTML = `<span>${city} > </span><span>${district} > </span><span>${title}</span>`;
-
-        const mainImgUrl = jsonData.firstimage;
-        imageUrls.push(mainImgUrl);
-
-        // 추가 이미지를 가져오고 슬라이더에 반영
-        getImage(imgUrl).then(result => {
-            imageUrls.push(...result);
-            updateImages();
-        });
-
-        contentTypeId = jsonData.contenttypeid;
-        document.querySelector('.title').innerText = jsonData.title
-        document.querySelector('.locationInfo').innerHTML = `주소 : ${jsonData.addr1}`;
-
-        getIntroInfo(contentTypeId).then(result=>{
-            const introData = result.response.body.items.item[0];
-            document.querySelector('.telInfo').innerHTML = `전화번호 : ${introData.infocenter}`
-            document.querySelector('.restInfo').innerHTML = `쉬는날 : ${introData.restdate}`
-            document.querySelector('.timeInfo').innerHTML = `이용시간 : ${introData.usetime}`
-        })
-        getAdditionalInfo(contentTypeId).then(result => {
-            const additionalInfoData = result.response.body.items.item[0];
-            document.querySelector('.priceInfo').innerHTML = `${additionalInfoData.infoname} : ${additionalInfoData.infotext}`
-        })
-        document.querySelector('.homepageInfo').innerHTML = `홈페이지 : ${jsonData.homepage}`
-        document.querySelector('.details').innerHTML = `<p>소개</p><span>${jsonData.overview}</span>`;
-        mapx = jsonData.mapx;
-        mapy = jsonData.mapy;
-        //주변정보
-        // processNearbySightsAndFood(mapx, mapy).then(result => {
-        //     renderNearbySightsAndFood(result.sights, result.food);
-        // }).catch(error => {
-        //     console.log(error)
-        // });
-    });
-
-// 이미지 URL 배열을 동적으로 추가하는 함수
-function updateImages() {
-    imageUrlsDiv.innerHTML = '';
-    const visibleImages = imageUrls.slice(currentIndex, currentIndex + 4);
-
-    visibleImages.forEach(url => {
-        const imgElement = document.createElement('img');
-        imgElement.src = url;
-        imgElement.classList.add('slideImage');
-        imageUrlsDiv.appendChild(imgElement);
-    });
-}
-
-// 이전 버튼 클릭 시
-document.querySelector('.prev').addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-    } else {
-        currentIndex = imageUrls.length - 4;
-    }
-    updateImages();
-});
-
-// 다음 버튼 클릭 시
-document.querySelector('.next').addEventListener('click', () => {
-    if (currentIndex < imageUrls.length - 4) {
-        currentIndex++;
-    } else {
-        currentIndex = 0;
-    }
-    updateImages();
-});
+// fetch(detailInfoUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//         const jsonData = data.response.body.items.item[0];
+//         const locationDiv = document.querySelector('.location');
+//         let city = splitAddr(jsonData.addr1)[0];
+//         let district = splitAddr(jsonData.addr1)[1];
+//         let title = jsonData.title;
+//         locationDiv.innerHTML = `<span>${city} > </span><span>${district} > </span><span>${title}</span>`;
+//
+//         const mainImgUrl = jsonData.firstimage;
+//         imageUrls.push(mainImgUrl);
+//
+//         // 추가 이미지를 가져오고 슬라이더에 반영
+//         getImage(imgUrl).then(result => {
+//             imageUrls.push(...result);
+//             updateImages();
+//         });
+//
+//         contentTypeId = jsonData.contenttypeid;
+//         document.querySelector('.title').innerText = jsonData.title
+//         document.querySelector('.locationInfo').innerHTML = `주소 : ${jsonData.addr1}`;
+//
+//         getIntroInfo(contentTypeId).then(result=>{
+//             const introData = result.response.body.items.item[0];
+//             document.querySelector('.telInfo').innerHTML = `전화번호 : ${introData.infocenter}`
+//             document.querySelector('.restInfo').innerHTML = `쉬는날 : ${introData.restdate}`
+//             document.querySelector('.timeInfo').innerHTML = `이용시간 : ${introData.usetime}`
+//         })
+//         getAdditionalInfo(contentTypeId).then(result => {
+//             const additionalInfoData = result.response.body.items.item[0];
+//             document.querySelector('.priceInfo').innerHTML = `${additionalInfoData.infoname} : ${additionalInfoData.infotext}`
+//         })
+//         document.querySelector('.homepageInfo').innerHTML = `홈페이지 : ${jsonData.homepage}`
+//         document.querySelector('.details').innerHTML = `<p>소개</p><span>${jsonData.overview}</span>`;
+//         mapx = jsonData.mapx;
+//         mapy = jsonData.mapy;
+//         //주변정보
+//         processNearbySightsAndFood(mapx, mapy).then(result => {
+//             renderNearbySightsAndFood(result.sights, result.food);
+//         }).catch(error => {
+//             console.log(error)
+//         });
+//     });
 
 // 주소 처리 함수
 function splitAddr(address) {
@@ -111,6 +78,39 @@ async function getImage(url) {
     }
 }
 
+// 이미지 URL 배열을 동적으로 추가하는 함수
+function updateImages() {
+    imageUrlsDiv.innerHTML = '';
+    const visibleImages = imageUrls.slice(currentIndex, currentIndex + 5);
+
+    visibleImages.forEach(url => {
+        const imgElement = document.createElement('img');
+        imgElement.src = url;
+        imgElement.classList.add('slideImage');
+        imageUrlsDiv.appendChild(imgElement);
+    });
+}
+
+// 이전 버튼 클릭 시
+document.querySelector('.prev').addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = imageUrls.length - 5;
+    }
+    updateImages();
+});
+
+// 다음 버튼 클릭 시
+document.querySelector('.next').addEventListener('click', () => {
+    if (currentIndex < imageUrls.length - 5) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    updateImages();
+});
+
 //인트로정보 조회 함수
 async function getIntroInfo(contentTypeId) {
     try {
@@ -122,6 +122,7 @@ async function getIntroInfo(contentTypeId) {
         console.log(error)
     }
 }
+
 //추가정보 조회 함수
 async function getAdditionalInfo(contentTypeId) {
     try{
@@ -157,80 +158,107 @@ async function getNearByFoodInfo(mapx, mapy, radius){
         console.log(error);
     }
 }
+//리뷰관련 부분
+//리뷰등록
+async function writeReview() {
+    const data = {
+        reContent : document.querySelector('.reviewArea').value
+    };
+    try {
+        const url = '/place/review';
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            body: JSON.stringify(data)
+        };
+        const resp = await fetch(url, config);
+        const result = await resp.text();
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+document.querySelector('.addButton').addEventListener('click', ()=>{
+    console.log(document.querySelector('.reviewArea').value)
+    writeReview().then(result => console.log(result))
+})
 
 // 주변 관광지와 음식점 조회 후 조건 처리 함수
-// async function processNearbySightsAndFood(mapx, mapy) {
-//     let range = 1000;
-//     async function fetchAndProcess() {
-//         const [sightsArray = [], foodArray = []] = await Promise.all([
-//             getNearBySights(mapx, mapy, range).catch(() => []),
-//             getNearByFoodInfo(mapx, mapy, range).catch(() => [])
-//         ]);
-//         const filteredSights = (sightsArray || []).filter(sight => sight.firstimage);
-//         const filteredFood = (foodArray || []).filter(food => food.firstimage);
-//         if (filteredSights.length < 5 || filteredFood.length < 5) {
-//             range += 1000;
-//             return fetchAndProcess();
-//         }
-//         filteredSights.sort((a, b) => a.dist - b.dist);
-//         filteredFood.sort((a, b) => a.dist - b.dist);
-//
-//         const topSights = filteredSights.slice(1, 5);
-//         const topFood = filteredFood.slice(1, 5);
-//         return {
-//             sights: topSights,
-//             food: topFood
-//         };
-//     }
-//     return fetchAndProcess();
-// }
+async function processNearbySightsAndFood(mapx, mapy) {
+    let range = 1000;
+    async function fetchAndProcess() {
+        const [sightsArray = [], foodArray = []] = await Promise.all([
+            getNearBySights(mapx, mapy, range).catch(() => []),
+            getNearByFoodInfo(mapx, mapy, range).catch(() => [])
+        ]);
+        const filteredSights = (sightsArray || []).filter(sight => sight.firstimage);
+        const filteredFood = (foodArray || []).filter(food => food.firstimage);
+        if (filteredSights.length < 5 || filteredFood.length < 5) {
+            range += 1000;
+            return fetchAndProcess();
+        }
+        filteredSights.sort((a, b) => a.dist - b.dist);
+        filteredFood.sort((a, b) => a.dist - b.dist);
+
+        const topSights = filteredSights.slice(1, 5);
+        const topFood = filteredFood.slice(1, 5);
+        return {
+            sights: topSights,
+            food: topFood
+        };
+    }
+    return fetchAndProcess();
+}
 //주변 관광지, 음식점 화면출력 함수
-// function renderNearbySightsAndFood(sights, food) {
-//     foodContainer.innerHTML = '';
-//     attractionsContainer.innerHTML = '';
-//
-//     food.forEach(item => {
-//         const foodDiv = document.createElement('div');
-//         foodDiv.classList.add('locations');
-//         const img = document.createElement('img');
-//         img.src = item.firstimage;
-//         img.alt = item.title;
-//         foodDiv.appendChild(img);
-//         const infoDiv = document.createElement('div');
-//         infoDiv.classList.add('info');
-//         const titleP = document.createElement('p');
-//         titleP.classList.add('title');
-//         titleP.textContent = item.title;
-//         const distP = document.createElement('p');
-//         distP.classList.add('distance');
-//         distP.textContent = convertDistance(item.dist);
-//         infoDiv.appendChild(titleP);
-//         infoDiv.appendChild(distP);
-//         foodDiv.appendChild(infoDiv);
-//         foodContainer.appendChild(foodDiv);
-//     });
-//
-//     sights.forEach(item => {
-//         const sightDiv = document.createElement('div');
-//         sightDiv.classList.add('locations');
-//         const img = document.createElement('img');
-//         img.src = item.firstimage;
-//         img.alt = item.title;
-//         sightDiv.appendChild(img);
-//         const infoDiv = document.createElement('div');
-//         infoDiv.classList.add('info');
-//         const titleP = document.createElement('p');
-//         titleP.classList.add('title');
-//         titleP.textContent = item.title;
-//         const distP = document.createElement('p');
-//         distP.classList.add('distance');
-//         distP.textContent = convertDistance(item.dist);
-//         infoDiv.appendChild(titleP);
-//         infoDiv.appendChild(distP);
-//         sightDiv.appendChild(infoDiv);
-//         attractionsContainer.appendChild(sightDiv);
-//     });
-// }
+function renderNearbySightsAndFood(sights, food) {
+    foodContainer.innerHTML = '';
+    attractionsContainer.innerHTML = '';
+
+    food.forEach(item => {
+        const foodDiv = document.createElement('div');
+        foodDiv.classList.add('locations');
+        const img = document.createElement('img');
+        img.src = item.firstimage;
+        img.alt = item.title;
+        foodDiv.appendChild(img);
+        const infoDiv = document.createElement('div');
+        infoDiv.classList.add('info');
+        const titleP = document.createElement('p');
+        titleP.classList.add('title');
+        titleP.textContent = item.title;
+        const distP = document.createElement('p');
+        distP.classList.add('distance');
+        distP.textContent = convertDistance(item.dist);
+        infoDiv.appendChild(titleP);
+        infoDiv.appendChild(distP);
+        foodDiv.appendChild(infoDiv);
+        foodContainer.appendChild(foodDiv);
+    });
+
+    sights.forEach(item => {
+        const sightDiv = document.createElement('div');
+        sightDiv.classList.add('locations');
+        const img = document.createElement('img');
+        img.src = item.firstimage;
+        img.alt = item.title;
+        sightDiv.appendChild(img);
+        const infoDiv = document.createElement('div');
+        infoDiv.classList.add('info');
+        const titleP = document.createElement('p');
+        titleP.classList.add('title');
+        titleP.textContent = item.title;
+        const distP = document.createElement('p');
+        distP.classList.add('distance');
+        distP.textContent = convertDistance(item.dist);
+        infoDiv.appendChild(titleP);
+        infoDiv.appendChild(distP);
+        sightDiv.appendChild(infoDiv);
+        attractionsContainer.appendChild(sightDiv);
+    });
+}
+
 //거리 변환 함수
 function convertDistance(dist) {
     let number = parseFloat(dist);
