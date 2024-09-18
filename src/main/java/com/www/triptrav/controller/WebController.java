b96d43b1240fb7ab49d46f23122369818216e6c7
ehdwo13 <ehdwo13@gmail.com>
2024-09-19 01:07:27 +0900

    add search button to change display in index page logic

--- src/main/java/com/www/triptrav/controller/WebController.java
+++ src/main/java/com/www/triptrav/controller/WebController.java
@@ -1,9 +1,12 @@
 package com.www.triptrav.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
 public class WebController {
 
     @GetMapping("/react")
@@ -13,6 +16,11 @@ public class WebController {
 
     @GetMapping("/")
     public String index() {
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public String search(){
         return "index";
     }
 }
