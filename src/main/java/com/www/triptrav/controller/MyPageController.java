34cc072c4a9bc962cc63ba4a7d3b5c2cfba64dc1
wanted2001 <jimyungsla@naver.com>
2024-09-24 14:39:53 +0900

    마이페이지 작업중

--- src/main/java/com/www/triptrav/controller/MyPageController.java
+++ src/main/java/com/www/triptrav/controller/MyPageController.java
@@ -4,7 +4,9 @@ package com.www.triptrav.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,6 +15,11 @@ public class MyPageController {
 
     @GetMapping("/mypage")
     public void mypage() {}
+
+    @PostMapping("/mypage")
+    public String updateNickname(@RequestParam String nickname) {
+        return "redirect:/mypage?nickname=" + nickname;
+    }
 
     @GetMapping("/tripList")
     public void tripList() {}
