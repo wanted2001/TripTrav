18e582a05bdfac2527447a34a35cb20e6d6c186a
gajami-kim <qansd38@gmail.com>
2024-09-27 19:58:54 +0900

    plan => schedule / 유저 일정 떼오기

---
+++ src/main/java/com/www/triptrav/domain/ScheduleDetailVO.java
@@ -0,0 +1,13 @@
+package com.www.triptrav.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScheduleDetailVO {
+    private long sco;
+    private long scheContentId;
+    private int scheIndex;
+}
---
+++ src/main/java/com/www/triptrav/domain/ScheduleVO.java
@@ -0,0 +1,18 @@
+package com.www.triptrav.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ScheduleVO {
+    private long sco;
+    private long uno;
+    private String scheName;
+    private String scheStart;
+    private String scheEnd;
+    private int scheCount; //여행소요일수
+    private String scheMemo;
+}
