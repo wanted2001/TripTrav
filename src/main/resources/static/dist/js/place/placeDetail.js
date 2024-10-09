4b8e0c8d5c620027da47b4296a466938cd7245cb
wanted2001 <jimyungsla@naver.com>
2024-10-09 19:45:27 +0900

    음식 상세페이지 적용

--- src/main/resources/static/dist/js/place/placeDetail.js
+++ src/main/resources/static/dist/js/place/placeDetail.js
@@ -163,7 +163,7 @@ document.querySelector('.next').addEventListener('click', () => {
 //인트로정보 조회 함수
 async function getIntroInfo(contentTypeId) {
     try {
-        const url = `https://apis.data.go.kr/B551011/KorService1/detailIntro1?MobileOS=ETC&MobileApp=TripTrav&_type=json&contentId=126676&contentTypeId=${contentTypeId}&serviceKey=${tourAPIKEY}`;
+        const url = `https://apis.data.go.kr/B551011/KorService1/detailIntro1?MobileOS=ETC&MobileApp=TripTrav&_type=json&contentId=${contentId}&contentTypeId=${contentTypeId}&serviceKey=${tourAPIKEY}`;
         const response = await fetch(url);
         const result = await response.json()
         return result;
@@ -175,7 +175,7 @@ async function getIntroInfo(contentTypeId) {
 //추가정보 조회 함수
 async function getAdditionalInfo(contentTypeId) {
     try{
-        const url = `https://apis.data.go.kr/B551011/KorService1/detailInfo1?MobileOS=ETC&MobileApp=TripTrav&_type=json&contentId=126676&contentTypeId=${contentTypeId}&serviceKey=${tourAPIKEY}`
+        const url = `https://apis.data.go.kr/B551011/KorService1/detailInfo1?MobileOS=ETC&MobileApp=TripTrav&_type=json&contentId=${contentId}&contentTypeId=${contentTypeId}&serviceKey=${tourAPIKEY}`
         const response = await fetch(url);
         const result = await response.json()
         return result;
